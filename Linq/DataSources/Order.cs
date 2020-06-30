397695d15e28004a80ee81a913447529ffe80575
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-06-30 07:59:00 +0000

    Update Order.cs

--- Linq/DataSources/Order.cs
+++ Linq/DataSources/Order.cs
@@ -1,12 +1,15 @@
-﻿using System;
+using System;
 
 namespace Linq.DataSources
 {
     public class Order
     {
         public int OrderId { get; set; }
+        
         public DateTime OrderDate { get; set; }
+        
         public decimal Total { get; set; }
+        
         public override string ToString() => $"{OrderId}: {OrderDate:d} for {Total:C2}";
     }
 }
