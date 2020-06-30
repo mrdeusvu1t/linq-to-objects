9e1d87bd25f704c29abdc1d8917cb8edc0acac03
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-06-30 12:52:28 +0000

    Upload New File

---
+++ Linq.Tests/QueryExecutionTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using static Linq.QueryExecution;
+
+namespace Linq.Tests
+{
+    [TestFixture]
+    public class QueryExecutionTests
+    {
+        [Test]
+        public void LazyExecutionTest()
+        {
+            var expected = new List<int> {9, 8, 6, 7, 10};
+            
+            CollectionAssert.AreEqual(expected, LazyExecution());
+        }
+        
+        [Test]
+        public void EagerExecutionTest()
+        {
+            var expected = new List<int> {9, 8, 6, 7};
+            
+            CollectionAssert.AreEqual(expected, EagerExecution());
+        }
+    }
+}
