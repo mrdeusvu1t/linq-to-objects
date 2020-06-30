4cab9251580330e582f0a20a67a07ccd2f319154
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-06-30 07:55:45 +0000

    Update Product.cs

--- Linq/DataSources/Product.cs
+++ Linq/DataSources/Product.cs
@@ -1,17 +1,22 @@
-﻿using System;
+using System;
 
 namespace Linq.DataSources
 {
     public class Product
     {
         public int ProductId { get; set; }
+        
         public string ProductName { get; set; }
+        
         public string Category { get; set; }
+        
         public decimal UnitPrice { get; set; }
+        
         public int UnitsInStock { get; set; }
         
         public override string ToString() =>
             $"ProductId={ProductId} ProductName={ProductName} Category={Category} UnitPrice={UnitPrice:C2} UnitsInStock={UnitsInStock}";
+            
         public bool Equals(Product other)
         {
             return ProductId == other.ProductId && ProductName == other.ProductName && Category == other.Category && UnitPrice == other.UnitPrice && UnitsInStock == other.UnitsInStock;
